9f9e94b73b2392356fa2af02bed56c6a7c3187cd
Emanuel Lima <emanuel-lima@outlook.com>
2019-10-04 02:31:58 -0300

    Checa por números felizes

--- numero_feliz.py
+++ numero_feliz.py
@@ -1,8 +1,42 @@
 # Programa que decide se um dado número é feliz ou não.
-# Um número é feliz se ao se realizar uma soma dos quadrados dos digitos do número, obtém-se ao final o número 1. Não é feliz caso contrário.
+# Um número é feliz se ao se realizar uma soma dos quadrados dos digitos do número,
+# obtém-se ao final o número 1 (possivelmente após várias iterações).
+# Não é feliz caso contrário.
+
 
 def main():
-    ...
+    numero: str = input("Digite um número: ")
+
+    while not numero.isnumeric():
+        print("Entrada inválida. Digite apenas números!")
+        numero: str = input("Digite um número: ")
+
+    if feliz(numero):
+        print(f"{numero} é um número feliz.")
+    else:
+        print(f"{numero} não é um número feliz.")
+
+
+def digitos_quadrados(numero: str) -> int:
+    soma: int = 0
+
+    for digito in numero:
+        soma += int(digito) ** 2
+
+    return soma
+
+
+def feliz(numero: str) -> bool:
+    numeros_calculados: List[int] = []
+    soma_parcial: int = digitos_quadrados(numero)
+
+    # Todo número não-feliz passa pelo ciclo 4 → 16 → 37 → 58 → 89 → 145 → 42 → 20 → 4 → ...
+    # Então, achar uma repetição desse ciclo é uma condição de parada.
+    while soma_parcial > 1 and soma_parcial not in numeros_calculados:
+        numeros_calculados.append(soma_parcial)
+        soma_parcial = digitos_quadrados(str(soma_parcial))
+
+    return soma_parcial == 1
 
 if __name__ == "__main__":
     main()
